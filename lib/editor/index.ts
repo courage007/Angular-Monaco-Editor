9fdb7eeeef0f32dc59e73430410db4d248ded5d7
courage007 <wang_xufa@163.com>
2018-09-22 10:38:21 +0800

    Add index.ts file

---
+++ lib/editor/index.ts
@@ -0,0 +1,5 @@
+export { AngularMonacoEditorModule } from './editor.module';
+export { AngularMonacoEditorComponent }  from './angular-monaco-editor/angular-monaco-editor.component';
+export { AngularMonacoEditorConfig } from './config';
+export { CODE_EDITOR_EVENTS } from './constants/events';
+export { CodeEditorEventService } from './services/code-editor.event.service';
