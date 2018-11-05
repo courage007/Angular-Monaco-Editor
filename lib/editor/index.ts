bcc7769cefc6c13148bc6bea77dd1fc457d4260c
courage007 <wang_xufa@163.com>
2018-11-05 17:04:22 +0800

    Update index.ts file to export angular-monaco-editor service

--- lib/editor/index.ts
+++ lib/editor/index.ts
@@ -3,3 +3,4 @@ export *  from './angular-monaco-editor/angular-monaco-editor.component';
 export * from './model/config';
 export * from './constant/events';
 export * from './service/code-editor.event.service';
+export * from './service/angular-monaco-editor.service';
