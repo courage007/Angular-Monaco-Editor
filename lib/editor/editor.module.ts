8ce03ea811b0bcf6810705c91ff38a913cb70167
courage007 <wang_xufa@163.com>
2019-04-12 17:35:05 +0800

    Fix no AngularMonacoEditorService problem

--- lib/editor/editor.module.ts
+++ lib/editor/editor.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ANGULAR_MONACO_EDITOR_CONFIG, AngularMonacoEditorConfig } from './model/config';
 import { AngularMonacoEditorComponent } from './angular-monaco-editor/angular-monaco-editor.component';
+import { AngularMonacoEditorService } from './service/angular-monaco-editor.service';
 
 @NgModule({
   imports: [
@@ -15,6 +16,7 @@ import { AngularMonacoEditorComponent } from './angular-monaco-editor/angular-mo
     AngularMonacoEditorComponent
   ],
   providers: [
+    AngularMonacoEditorService
   ]
 })
 
